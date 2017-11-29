86d76a61d0673c33250ca731f6011025faf0a2d8
smilecc <sxcuican@163.com>
2017-11-29 18:36:35 +0800

    Added: 完成blive getroomid

---
+++ api/application/index/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return result(1, 'Hello world');
+    }
+
+    public function test ()
+    {
+        $blive = new \Blive\User('');
+        $blive->getRoomId();
+    }
+}
