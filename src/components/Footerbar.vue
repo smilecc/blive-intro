5652781fa75bfb89951e56060d2a5900d1d51fd9
smilecc <sxcuican@163.com>
2017-12-01 18:02:07 +0800

    Modify: 底部增加Github与QQ群

--- src/components/Footerbar.vue
+++ src/components/Footerbar.vue
@@ -1,14 +1,19 @@
 <template>
     <footer>
-        Developed By <a href="https://github.com/smilecc" target="_blank">Can</a>.
+        <div class="title">Developed By <a href="https://github.com/smilecc" target="_blank">Can</a>.</div>
+        <div class="desc">
+            <span>Github：<a href="https://github.com/smilecc/blive-intro" target="_blank">欢迎Star与PR</a></span>
+            	&nbsp;	&nbsp;
+            <span>反馈QQ群：680299588</span>
+        </div>
     </footer>
 </template>
 
 <style scoped>
     footer {
         margin-top: 90px;
-        height: 60px;
-        background-color: #AAA;
+        height: 90px;
+        background-color: #2e3033;
         font-size: 18px;
         color: white;
         padding-top: 17px;
@@ -16,5 +21,15 @@
     }
     a {
         color: #ddd !important;
+    }
+    a:hover {
+        color: #00a4ff !important;
+    }
+    .title {
+        padding-top: 5px;
+    }
+    .desc {
+        font-size: 14px;
+        padding-top: 5px;
     }
 </style>
